c4303eb95b43f9ab74b56e24207d76360287dd46
dineshdsv3 <dineshdsv3@gmail.com>
2020-06-13 11:35:37 +0530

    withdraw by seller  function created in smart contract

--- src/contracts/Escrow.sol
+++ src/contracts/Escrow.sol
@@ -6,9 +6,9 @@ contract Escrow {
 
     struct EscrowProduct {
         uint256 id;
-        address seller;
-        address buyer;
-        address judge; // Setting judge address by the system based on consensus
+        address payable seller;
+        address payable buyer;
+        address payable judge; // Setting judge address by the system based on consensus
         uint256 amount;
         uint256 judge_fee;
         uint256 timestamp;
@@ -40,15 +40,31 @@ contract Escrow {
     function purchaseProduct(uint _id) public payable {
         EscrowProduct memory productModel = products[_id];
         require(productModel.available = true,'Product must be available for sale');
-        require(msg.value >= 0,'Smart contract value must  be greater than 0');
+        require(msg.value >= productModel.amount,'Smart contract value must  be greater or equal to the product amount');
 
         productModel.buyer = msg.sender; // Setting the buyer for the product
 
         productModel.available = false;  //As buyer initiated this transaction this is set to false temporarily
 
-        productModel.dispute_time = now + 86400; // Dispute time initiated upto 24 hours from now
+        productModel.dispute_time = now + 40; // Dispute time initiated upto 24 hours from now
 
         products[_id] = productModel; // changing the orginal product again by using memory
+    }
+
+    function withdrawBySeller(uint _id) public payable {
+        EscrowProduct memory productModel = products[_id];
+        require(now > productModel.dispute_time,'Seller cant be able to withdraw his funds before dispute time');
+         address payable _seller = productModel.seller;
+        require(msg.sender == _seller,'Only sender can be able to perform this function');
+        if(now > productModel.dispute_time && !productModel.judge_intervention) {
+            productModel.release_amount_seller = true;
+        } else{
+            productModel.release_amount_seller = false;
+        }
+        products[_id] = productModel;
+        require(productModel.release_amount_seller = true,'Release amount must be true to withdraw the amount');
+
+        productModel.seller.transfer(productModel.amount); // Transfer product amount to the seller of the product
     }
 
 }
